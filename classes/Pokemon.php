5f60ed2fe90250e9ed9356b6e0af7ea933ea1caa
Job <99058203@mydavinci.nl>
2021-09-02 11:52:31 +0200

    made pokemon class (unfinished, sort of)
    made the first class (pokemon.php).
    Still have to test the code.

---
+++ classes/Pokemon.php
@@ -0,0 +1,26 @@
+<?php
+class Pokemon{
+    public $name;
+    public $energytype;
+    public $hitpoints;
+    public $health;
+    public $attacks;
+    public $weakness;
+    public $resistance;
+
+    public function __construct($name, $energytype, $hitpoints, $health, $attacks, $weakness, $resistance){
+        $this->name = $name;
+        $this->energytype = $energytype;
+        $this->hitpoints = $hitpoints;
+        $this->health = $health;
+        $this->attacks = $attacks;
+        $this->weakness = $weakness;
+        $this->resistance = $resistance;
+    }
+
+    public function __toString() {
+        return json_encode($this);
+    }
+}
+
+?>
