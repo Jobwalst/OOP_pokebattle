51b625d6bc17bf3b553a8776dbe22152beb42890
Job <99058203@mydavinci.nl>
2022-01-20 11:27:59 +0100

    removed unecessary comments
    removed comments from index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 require('functions.php');
 
-$pika = new Pikachu('Pika'); //using the 'new' keyword a new instance of the corresponding class gets made
+$pika = new Pikachu('Pika');
 $char = new Charmeleon('Char');
 
-echo Pokemon::getPopulation(); //using X::y you can call a static method
+echo Pokemon::getPopulation(); 
 
 echo '<br>' . $pika->attack($char, $pika->getAttacks()[0], $pika->getEnergyType());
 echo '<br>' . $char->attack($pika, $char->getAttacks()[1], $char->getEnergyType());
